2307b8925bcc71da4bc59da03f24cbe329abee8d
Avaneesh Sundararajan <avaneeshsundar@gmail.com>
2024-09-11 09:56:03 +0530

    testing full tpr with min d calc

--- parallel/multi_PIHC_2opt.cu
+++ parallel/multi_PIHC_2opt.cu
@@ -397,16 +397,16 @@ int main(int argc, char *argv[])
 
 	printf("\n tpr finished running");
 
-	for(int itr=0;itr<cities;itr++)
-	{
-		// d[itr] = dtid[itr] >> 32;
-		printf("hello");
-		printf("\n %llu",dtid[itr]);
-		// if(d[itr]<min_d)
-		// {
-		// 	min_d=d[itr];
-		// }
-	}
+	// for(int itr=0;itr<cities;itr++)
+	// {
+	// 	// d[itr] = dtid[itr] >> 32;
+	// 	printf("hello");
+	// 	printf("\n %llu",dtid[itr]);
+	// 	// if(d[itr]<min_d)
+	// 	// {
+	// 	// 	min_d=d[itr];
+	// 	// }
+	// }
 
 	printf("\n first tpr call complete moved min d");
 	
